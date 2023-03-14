b0b6074a7d73e673f2afbc08daca2419c5cb73e2
SafetyFlux <dekuwither@gmail.com>
2023-03-14 10:08:29 -0600

    Implement stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import StartScreen from './screens/StartScreen';
+import GameScreen from './screens/GameScreen';
+import EndScreen from './screens/EndScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="StartScreen"
+            component={StartScreen}
+          />
+          <Stack.Screen
+            name="GameScreen"
+            component={GameScreen}
+          />
+          <Stack.Screen
+            name="EndScreen"
+            component={EndScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
