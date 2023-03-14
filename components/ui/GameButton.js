876af266261f380bc02bf7e2e26be6df4a5f8621
SafetyFlux <dekuwither@gmail.com>
2023-03-14 10:47:13 -0600

    Style UI components

--- components/ui/GameButton.js
+++ components/ui/GameButton.js
@@ -1,5 +1,28 @@
-function GameButton() {
+import { Pressable, Text, StyleSheet } from 'react-native';
 
+function GameButton({ children, onPress }) {
+  return (
+    <Pressable style={styles.buttonContainer} onPress={onPress}>
+      <Text style={styles.buttonText}>{children}</Text>
+    </Pressable>
+  );
 }
 
 export default GameButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: 'white',
+    marginVertical: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+  },
+});
