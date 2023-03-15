a80e87fb8dcac7c999358be45b7a9c4638714d3b
SafetyFlux <dekuwither@gmail.com>
2023-03-15 09:38:35 -0600

    Change status bar style

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
