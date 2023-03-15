5be1899569b43de21191e488e14e1b51b7c908c0
SafetyFlux <dekuwither@gmail.com>
2023-03-15 12:25:31 -0600

    Add navigation to start screen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -3,11 +3,13 @@ import { View, StyleSheet } from 'react-native';
 import Title from '../components/ui/Title';
 import GameButton from '../components/ui/GameButton';
 
-function StartScreen() {
+function StartScreen({ navigation }) {
+  // Navigate to the game screen
   function startGameHandler() {
-    
+    navigation.navigate('GameScreen');
   }
 
+  // Return the screen view
   return (
     <View style={styles.container}>
       <Title>Welcome to the Color Guessing Game! Press the button to begin.</Title>
